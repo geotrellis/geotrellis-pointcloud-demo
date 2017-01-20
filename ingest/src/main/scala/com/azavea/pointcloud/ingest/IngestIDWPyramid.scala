0841318dbf42567746b8168b75bcabfa9b197117
Grigory Pomadchin <gr.pomadchin@gmail.com>
2017-01-20 22:24:52 +0300

    TIN & IDW ingest jobs

---
+++ ingest/src/main/scala/com/azavea/pointcloud/ingest/IngestIDWPyramid.scala
@@ -0,0 +1,158 @@
+package com.azavea.pointcloud.ingest
+
+import com.azavea.pointcloud.ingest.conf.IngestConf
+
+import geotrellis.pointcloud.pipeline.{RangeFilter, Read, ReprojectionFilter}
+import geotrellis.pointcloud.spark._
+import geotrellis.pointcloud.spark.dem.{PointCloudToDem, PointToGrid}
+import geotrellis.pointcloud.spark.io.hadoop._
+import geotrellis.pointcloud.spark.tiling.CutPointCloud
+import geotrellis.proj4.CRS
+import geotrellis.raster.io._
+import geotrellis.raster.io.geotiff.GeoTiff
+import geotrellis.raster._
+import geotrellis.spark._
+import geotrellis.spark.io._
+import geotrellis.spark.io.hadoop._
+import geotrellis.spark.io.index.ZCurveKeyIndexMethod
+import geotrellis.spark.io.kryo.KryoRegistrator
+import geotrellis.spark.pyramid.Pyramid
+import geotrellis.spark.tiling._
+import geotrellis.util._
+import geotrellis.vector._
+
+import org.apache.hadoop.fs.Path
+import org.apache.spark.serializer.KryoSerializer
+import org.apache.spark.{SparkConf, SparkContext}
+
+object IngestIDWPyramid {
+  def main(args: Array[String]): Unit = {
+    val opts      = IngestConf.parse(args)
+    // val chunkPath = System.getProperty("user.dir") + "/chunks/"
+
+    val conf = new SparkConf()
+      .setIfMissing("spark.master", "local[*]")
+      .setAppName("PointCloudCount")
+      .set("spark.local.dir", "/data/spark")
+      .set("spark.serializer", classOf[KryoSerializer].getName)
+      .set("spark.kryo.registrator", classOf[KryoRegistrator].getName)
+
+    implicit val sc = new SparkContext(conf)
+
+    try {
+      val options = HadoopPointCloudRDD.Options.DEFAULT.copy(
+        pipeline =
+          Read("", opts.inputCrs) ~
+            ReprojectionFilter(opts.destCrs) ~
+            RangeFilter(Some(s"Z[0:${opts.maxValue}]"))
+      )
+
+      val source = HadoopPointCloudRDD(new Path(opts.inputPath), options).cache()
+
+      val (extent, crs) =
+        source
+          .map { case (header, _) => (header.projectedExtent3D.extent3d.toExtent, header.crs) }
+          .reduce { case ((e1, c), (e2, _)) => (e1.combine(e2), c) }
+
+      val targetCrs = CRS.fromName(opts.destCrs)
+
+      val targetExtent =
+        opts.extent match {
+          case Some(e) => if (crs.epsgCode != targetCrs.epsgCode) e.reproject(crs, targetCrs) else e
+          case _ =>  if (crs.epsgCode != targetCrs.epsgCode) extent.reproject(crs, targetCrs) else extent
+        }
+
+      val layoutScheme = if (opts.pyramid || opts.zoomed) ZoomedLayoutScheme(targetCrs) else FloatingLayoutScheme(512)
+
+      val LayoutLevel(zoom, layout) = layoutScheme.levelFor(targetExtent, opts.cellSize)
+      val kb = KeyBounds(layout.mapTransform(targetExtent))
+      val md = TileLayerMetadata[SpatialKey](FloatConstantNoDataCellType, layout, targetExtent, targetCrs, kb)
+
+      val tiled =
+        CutPointCloud(
+          source.flatMap(_._2),
+          layout
+        ).withContext {
+          _.reduceByKey({ (p1, p2) => p1 union p2 }, opts.numPartitions)
+        }
+
+      val tiles =
+        PointCloudToDem(
+          tiled, opts.cellSize,
+          PointToGrid.Options(cellType = FloatConstantNoDataCellType)
+        )
+
+      val layer = ContextRDD(tiles, md)
+
+      layer.cache()
+
+      def buildPyramid(zoom: Int, rdd: TileLayerRDD[SpatialKey])
+                      (sink: (TileLayerRDD[SpatialKey], Int) => Unit): List[(Int, TileLayerRDD[SpatialKey])] = {
+        if (zoom >= opts.minZoom) {
+          rdd.cache()
+          sink(rdd, zoom)
+          val pyramidLevel @ (nextZoom, nextRdd) = Pyramid.up(rdd, layoutScheme, zoom)
+          pyramidLevel :: buildPyramid(nextZoom, nextRdd)(sink)
+        } else {
+          sink(rdd, zoom)
+          List((zoom, rdd))
+        }
+      }
+
+      if(opts.persist) {
+        val writer = HadoopLayerWriter(new Path(opts.catalogPath))
+        val attributeStore = writer.attributeStore
+
+        var savedHisto = false
+        if (opts.pyramid) {
+          buildPyramid(zoom, layer) { (rdd, zoom) =>
+            writer
+              .write[SpatialKey, Tile, TileLayerMetadata[SpatialKey]](
+              LayerId(opts.layerName, zoom),
+              rdd,
+              ZCurveKeyIndexMethod
+            )
+
+            println(s"=============================INGEST ZOOM LVL: $zoom=================================")
+
+            if (!savedHisto) {
+              savedHisto = true
+              val histogram = rdd.histogram(512)
+              attributeStore.write(
+                LayerId(opts.layerName, 0),
+                "histogram",
+                histogram
+              )
+            }
+          }.foreach { case (z, rdd) => rdd.unpersist(true) }
+        } else {
+          writer
+            .write[SpatialKey, Tile, TileLayerMetadata[SpatialKey]](
+            LayerId(opts.layerName, 0),
+            layer,
+            ZCurveKeyIndexMethod
+          )
+
+          if (!savedHisto) {
+            savedHisto = true
+            val histogram = layer.histogram(512)
+            attributeStore.write(
+              LayerId(opts.layerName, 0),
+              "histogram",
+              histogram
+            )
+          }
+        }
+      }
+
+      if(opts.testOutput.nonEmpty) {
+        val raster = layer.stitch
+        GeoTiff(raster, crs).write(opts.testOutput)
+      } else layer.count
+
+      layer.unpersist(blocking = false)
+      source.unpersist(blocking = false)
+
+    } finally sc.stop()
+  }
+}
---
+++ ingest/src/main/scala/com/azavea/pointcloud/ingest/conf/IngestConf.scala
@@ -0,0 +1,103 @@
+package com.azavea.pointcloud.ingest.conf
+
+import geotrellis.raster.CellSize
+import geotrellis.vector.Extent
+
+import org.apache.spark.SparkContext
+
+object IngestConf {
+  case class Options(
+    inputPath: String = "file:///data/test/",
+    catalogPath: String = "file:///data/catalog2",
+    testOutput: String = "/data/idw-ingest-out.tif",
+    layerName: String = "elevation",
+    persist: Boolean = false,
+    pyramid: Boolean = false,
+    zoomed: Boolean = false,
+    cellSize: CellSize = CellSize(0.5, 0.5),
+    numPartitions: Int = 5000,
+    minZoom: Int = 7,
+    maxValue: Int = 400,
+    destCrs: String = "EPSG:3857",
+    extent: Option[Extent] = None,
+    inputCrs: Option[String] = None
+  )
+
+  val help = """
+               |geotrellis-pointcloud-ingest
+               |
+               |Usage: geotrellis-pointcloud-ingest [options]
+               |
+               |  --inputPath <value>
+               |        inputPath is a non-empty String property [default: file:///data/test/]
+               |  --catalogPath <value>
+               |        catalogPath is a non-empty String property [default: file:///data/catalog2]
+               |  --layerName <value>
+               |        layerName is a non-empty String property [default: elevation]
+               |  --extent <value>
+               |        extent is a non-empty String in LatLng
+               |  --inputCrs <value>
+               |        inputCrs is a non-empty String
+               |  --destCrs <value>
+               |        destCrs is a non-empty String [default: EPSG:3857]
+               |  --persist <value>
+               |        persist is a a boolean option [default: false]
+               |  --pyramid <value>
+               |        pyramid is a boolean option [default: false]
+               |  --zoomed <value>
+               |        zoomed is a boolean option [default: false]
+               |  --cellSize <value>
+               |        cellSize is a non-empty String [default: 0.5,0.5]
+               |  --numPartitions <value>
+               |        numPatition is an integer value [default: 5000]
+               |  --minZoom <value>
+               |        minZoom is an integer value [default: 7]
+               |  --maxValue <value>
+               |        maxValue is an integer value [default: 400]
+               |  --help
+               |        prints this usage text
+             """.stripMargin
+
+  def nextOption(opts: Options, list: Seq[String]): Options = {
+    list.toList match {
+      case Nil => opts
+      case "--inputPath" :: value :: tail =>
+        nextOption(opts.copy(inputPath = value), tail)
+      case "--catalogPath" :: value :: tail =>
+        nextOption(opts.copy(catalogPath = value), tail)
+      case "--layerName" :: value :: tail =>
+        nextOption(opts.copy(layerName = value), tail)
+      case "--extent" :: value :: tail =>
+        nextOption(opts.copy(extent = Some(Extent.fromString(value))), tail)
+      case "--inputCrs" :: value :: tail =>
+        nextOption(opts.copy(inputCrs = Some(value)), tail)
+      case "--destCrs" :: value :: tail =>
+        nextOption(opts.copy(destCrs = value), tail)
+      case "--pyramid" :: value :: tail =>
+        nextOption(opts.copy(pyramid = value.toBoolean), tail)
+      case "--zoomed" :: value :: tail =>
+        nextOption(opts.copy(zoomed = value.toBoolean), tail)
+      case "--cellSize" :: value :: tail =>
+        nextOption(opts.copy(cellSize = CellSize.fromString(value)), tail)
+      case "--numPartitions" :: value :: tail =>
+        nextOption(opts.copy(numPartitions = value.toInt), tail)
+      case "--minZoom" :: value :: tail =>
+        nextOption(opts.copy(minZoom = value.toInt), tail)
+      case "--maxValue" :: value :: tail =>
+        nextOption(opts.copy(maxValue = value.toInt), tail)
+      case "--help" :: tail => {
+        println(help)
+        sys.exit(1)
+      }
+      case option :: tail => {
+        println(s"Unknown option ${option}")
+        println(help)
+        sys.exit(1)
+      }
+    }
+  }
+
+  def parse(args: Seq[String]) = nextOption(Options(), args)
+
+  def apply(args: Seq[String]): Options = parse(args)
+}
