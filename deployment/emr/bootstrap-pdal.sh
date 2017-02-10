0699d711f931b33053ea203035db53b7ea6e36c7
lossyrob <rdemanuele@gmail.com>
2017-02-10 16:32:13 -0500

    Update bootstrap to pull from s3

--- deployment/emr/bootstrap-pdal.sh
+++ deployment/emr/bootstrap-pdal.sh
@@ -9,64 +9,12 @@ aws s3 cp /etc/hadoop/conf/yarn-site.xml s3://geotrellis-test/pdal-test/
 # Install minimal explicit dependencies.
 sudo yum -y install git geos-devel libcurl-devel cmake libtiff-devel
 
-# laz-perf
+# Install binaries for pdal
 cd /mnt
-git clone https://github.com/verma/laz-perf.git laz-perf
-cd laz-perf
-cmake .
-make
-sudo make install
-
-# laszip
-cd /mnt
-git clone https://github.com/LASzip/LASzip.git laszip
-cd laszip
-git checkout e7065cbc5bdbbe0c6e50c9d93d1cd346e9be6778  # Yes this is necessary. See https://github.com/PDAL/PDAL/issues/1205
-cmake .
-make
-sudo make install
-
-# proj4
-cd /mnt
-wget https://github.com/OSGeo/proj.4/archive/4.9.3.zip
-unzip 4.9.3.zip
-cd proj.4-4.9.3
-cmake .
-make
-sudo make install
-
-# libgeotiff
-cd /mnt
-wget http://download.osgeo.org/geotiff/libgeotiff/libgeotiff-1.4.2.zip
-unzip libgeotiff-1.4.2.zip
-cd libgeotiff-1.4.2
-cmake .
-make
-sudo make install
-
-# jsoncpp
-cd /mnt
-wget https://github.com/open-source-parsers/jsoncpp/archive/1.7.7.zip
-unzip 1.7.7.zip
-cd jsoncpp-1.7.7
-cmake . -DBUILD_SHARED_LIBS=ON  # Need BUILD_SHARED_LIBS or pdal fails.
-make
-sudo make install
-
-# Compile/install GDAL
-cd /mnt
-git clone https://github.com/OSGeo/gdal.git
-git checkout 2.1
-cd gdal/gdal
-./configure
-make
-sudo make install
-
-# Compile/install PDAL
-cd /mnt
-git clone https://github.com/PDAL/PDAL.git pdal
-cd pdal
-git checkout -f 1.4-maintenance
-cmake . -DWITH_PDAL_JNI=ON -DWITH_APPS=ON -DWITH_LAZPERF=ON -DWITH_LASZIP=ON -DCMAKE_BUILD_TYPE=Release
-make -j4
-sudo make install
+aws s3 cp s3://geotrellis-demo/emr/pdal-binaries-v1.tar.gz .
+tar -xf pdal-binaries-v1.tar.gz
+cd pdal-binaries
+sudo mv pdal /usr/local/bin/pdal
+sudo mv gdal /usr/local/share/gdal
+cd lib
+sudo cp -r * /usr/local/lib
