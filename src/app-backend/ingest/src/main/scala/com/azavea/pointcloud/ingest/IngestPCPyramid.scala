46d66b8e562f5e91471b265d6ce164e8dbb44b9a
Grigory Pomadchin <gr.pomadchin@gmail.com>
2017-02-20 11:29:51 +0300

    refactor pc pyramid ingest

--- src/app-backend/ingest/src/main/scala/com/azavea/pointcloud/ingest/IngestPCPyramid.scala
+++ src/app-backend/ingest/src/main/scala/com/azavea/pointcloud/ingest/IngestPCPyramid.scala
@@ -1,14 +1,14 @@
 package com.azavea.pointcloud.ingest
 
 import com.azavea.pointcloud.ingest.conf.IngestConf
-import com.vividsolutions.jts.geom.Coordinate
 import io.pdal._
+
 import geotrellis.pointcloud.pipeline._
 import geotrellis.pointcloud.spark._
 import geotrellis.pointcloud.spark.io._
 import geotrellis.pointcloud.spark.io.hadoop._
 import geotrellis.pointcloud.spark.io.s3._
-import geotrellis.pointcloud.spark.tiling.Implicits.{withTilerMethods => withPCTilerMethods}
+import geotrellis.pointcloud.spark.pyramid._
 import geotrellis.proj4.CRS
 import geotrellis.raster._
 import geotrellis.spark._
@@ -20,18 +20,17 @@ import geotrellis.spark.io.s3.S3LayerWriter
 import geotrellis.spark.tiling._
 import geotrellis.util._
 import geotrellis.vector._
-import geotrellis.vector.triangulation._
+
 import org.apache.hadoop.fs.Path
 import org.apache.spark.rdd.RDD
 import org.apache.spark.serializer.KryoSerializer
 import org.apache.spark.{SparkConf, SparkContext}
 import spire.syntax.cfor.cfor
+import com.vividsolutions.jts.geom.Coordinate
 
 import scala.collection.mutable
 
 object IngestPCPyramid {
-  type PointCloudLayerRDD[K] = RDD[(SpatialKey, Array[Coordinate])] with Metadata[TileLayerMetadata[K]]
-
   def main(args: Array[String]): Unit = {
     val opts      = IngestConf.parse(args)
     // val chunkPath = System.getProperty("user.dir") + "/chunks/"
@@ -114,7 +113,7 @@ object IngestPCPyramid {
           .reduceByKey({ (p1, p2) => p1 ++ p2 }, opts.numPartitions)
           .filter { _._2.length > 2 }
 
-      val layer: PointCloudLayerRDD[SpatialKey] = ContextRDD(cut, md)
+      val layer = ContextRDD(cut, md)
 
       // layer.cache()
 
@@ -124,92 +123,8 @@ object IngestPCPyramid {
         if (zoom >= opts.minZoom) {
           // rdd.cache()
           sink(rdd, zoom)
-          val LayoutLevel(nextZoom, nextLayout) = layoutScheme.zoomOut(LayoutLevel(zoom, layout))
-
-          val nextKeyBounds =
-            kb match {
-              //case EmptyBounds => EmptyBounds
-              case kb: KeyBounds[SpatialKey] =>
-                // If we treat previous layout as extent and next layout as tile layout we are able to hijack MapKeyTransform
-                // to translate the spatial component of source KeyBounds to next KeyBounds
-                val extent = layout.extent
-                val sourceRe = RasterExtent(extent, layout.layoutCols, layout.layoutRows)
-                val targetRe = RasterExtent(extent, nextLayout.layoutCols, nextLayout.layoutRows)
-                val SpatialKey(sourceColMin, sourceRowMin) = kb.minKey.getComponent[SpatialKey]
-                val SpatialKey(sourceColMax, sourceRowMax) = kb.maxKey.getComponent[SpatialKey]
-                val (colMin, rowMin) = {
-                  val (x, y) = sourceRe.gridToMap(sourceColMin, sourceRowMin)
-                  targetRe.mapToGrid(x, y)
-                }
-
-                val (colMax, rowMax) = {
-                  val (x, y) = sourceRe.gridToMap(sourceColMax, sourceRowMax)
-                  targetRe.mapToGrid(x, y)
-                }
-
-                KeyBounds(
-                  kb.minKey.setComponent(SpatialKey(colMin, rowMin)),
-                  kb.maxKey.setComponent(SpatialKey(colMax, rowMax)))
-            }
-
-          val nextMetadata =
-            rdd.metadata
-              .setComponent(nextLayout)
-              .setComponent(nextKeyBounds)
-
-          // Functions for combine step
-          def createTiles(tile: (SpatialKey, Array[Coordinate])): Seq[(SpatialKey, Array[Coordinate])]                             = Seq(tile)
-          def mergeTiles1(tiles: Seq[(SpatialKey, Array[Coordinate])], tile: (SpatialKey, Array[Coordinate])): Seq[(SpatialKey, Array[Coordinate])]         = tiles :+ tile
-          def mergeTiles2(tiles1: Seq[(SpatialKey, Array[Coordinate])], tiles2: Seq[(SpatialKey, Array[Coordinate])]): Seq[(SpatialKey, Array[Coordinate])] = tiles1 ++ tiles2
-
-          val nextRdd = {
-            val transformedRdd = rdd
-              .map { case (key, arr) =>
-                val extent = layout.mapTransform(key)
-                val newSpatialKey = nextLayout.mapTransform(extent.center)
-                (key.setComponent(newSpatialKey), (key, arr))
-              }
-
-            transformedRdd.combineByKey(createTiles, mergeTiles1, mergeTiles2)
-              .map { case (newKey: SpatialKey, seq: Seq[(SpatialKey, Array[Coordinate])]) =>
-                val pts = seq.flatMap(_._2).toArray
-                val length = pts.length
-                val by = (0.75 * length).toInt
-                val newLength = length - by
-                val arr = new Array[Coordinate](newLength)
-                val dt = DelaunayTriangulation(pts)
-                dt.decimate(by)
-
-                val ps = dt.pointSet
-                cfor(0)(_ < newLength, _ + 1) { i =>
-                  arr(i) = ps.getCoordinate(i)
-                }
-
-                newKey -> arr
-              }
-          }
-
-          val nextCRdd = new ContextRDD(nextRdd, nextMetadata)
-
-          (nextZoom, nextCRdd) :: buildPyramid(nextZoom, nextCRdd)(sink)
-
-          /*val nextRdd = rdd.map { case (key, pts) =>
-            val length = pts.length
-            val dt = DelaunayTriangulation(pts)
-            dt.decimate((0.75 * length).toInt)
-
-            val ps = dt.pointSet
-            val arr = new Array[Coordinate](length)
-            cfor(0)(_ < length, _ + 1) { i =>
-              arr(i) = ps.getCoordinate(i)
-            }
-            
-            key -> arr
-          }*/
-
-          // buildPyramid(nextZoom, nextRdd)(sink)
-          // val pyramidLevel @ (nextZoom, nextRdd) = Pyramid.up(rdd, layoutScheme, zoom, Pyramid.Options(Bilinear))
-          // pyramidLevel :: buildPyramid(nextZoom, nextRdd)(sink)
+          val pyramidLevel @ (nextZoom, nextRdd) = Pyramid.up(rdd, layoutScheme, zoom, Pyramid.Options(opts.decimation))
+          pyramidLevel :: buildPyramid(nextZoom, nextRdd)(sink)
         } else {
           sink(rdd, zoom)
           List((zoom, rdd))
@@ -222,15 +137,24 @@ object IngestPCPyramid {
           else S3LayerWriter(opts.S3CatalogPath._1, opts.S3CatalogPath._2)
 
         if (opts.pyramid) {
+          buildPyramid(zoom, layer) { (rdd, zoom) =>
+            writer
+              .write[SpatialKey, Array[Coordinate], TileLayerMetadata[SpatialKey]](
+              LayerId(opts.layerName, zoom),
+              rdd,
+              ZCurveKeyIndexMethod
+            )
 
-        }
-
-        writer
-          .write[SpatialKey, PointCloud, TileLayerMetadata[SpatialKey]](
+            println(s"=============================INGEST ZOOM LVL: $zoom=================================")
+          }.foreach { case (z, rdd) => rdd.unpersist(true) }
+        } else {
+          writer
+            .write[SpatialKey, Array[Coordinate], TileLayerMetadata[SpatialKey]](
             LayerId(opts.layerName, 0),
             layer,
             ZCurveKeyIndexMethod
           )
+        }
       } else layer.count()
 
       layer.unpersist(blocking = false)
---
+++ src/app-backend/ingest/src/main/scala/geotrellis/pointcloud/spark/pyramid/Pyramid.scala
@@ -0,0 +1,214 @@
+package geotrellis.pointcloud.spark.pyramid
+
+import geotrellis.spark._
+import geotrellis.spark.tiling._
+import geotrellis.raster._
+import geotrellis.util._
+
+import org.apache.spark.Partitioner
+import org.apache.spark.rdd._
+import com.typesafe.scalalogging.LazyLogging
+import com.vividsolutions.jts.geom.Coordinate
+import geotrellis.vector.triangulation.DelaunayTriangulation
+import spire.syntax.cfor.cfor
+
+import scala.reflect.ClassTag
+
+object Pyramid extends LazyLogging {
+  case class Options(decimation: Double = 0.75, partitioner: Option[Partitioner] = None)
+  object Options {
+    def DEFAULT = Options()
+    implicit def partitionerToOptions(p: Partitioner): Options = Options(partitioner = Some(p))
+    implicit def optPartitionerToOptions(p: Option[Partitioner]): Options = Options(partitioner = p)
+    implicit def decimationToOptions(d: Double): Options = Options(decimation = d)
+  }
+
+  def up[
+    K: SpatialComponent: ClassTag,
+    M: Component[?, LayoutDefinition]: Component[?, Bounds[K]]
+  ](rdd: RDD[(K, Array[Coordinate])] with Metadata[M],
+    layoutScheme: LayoutScheme,
+    zoom: Int,
+    options: Options
+   ): (Int, RDD[(K,  Array[Coordinate])] with Metadata[M]) = {
+    val Options(decimation, partitioner) = options
+
+    val sourceLayout = rdd.metadata.getComponent[LayoutDefinition]
+    val sourceBounds = rdd.metadata.getComponent[Bounds[K]]
+    val LayoutLevel(nextZoom, nextLayout) = layoutScheme.zoomOut(LayoutLevel(zoom, sourceLayout))
+
+    val nextKeyBounds =
+      sourceBounds match {
+        case EmptyBounds => EmptyBounds
+        case kb: KeyBounds[K] =>
+          // If we treat previous layout as extent and next layout as tile layout we are able to hijack MapKeyTransform
+          // to translate the spatial component of source KeyBounds to next KeyBounds
+          val extent = sourceLayout.extent
+          val sourceRe = RasterExtent(extent, sourceLayout.layoutCols, sourceLayout.layoutRows)
+          val targetRe = RasterExtent(extent, nextLayout.layoutCols, nextLayout.layoutRows)
+          val SpatialKey(sourceColMin, sourceRowMin) = kb.minKey.getComponent[SpatialKey]
+          val SpatialKey(sourceColMax, sourceRowMax) = kb.maxKey.getComponent[SpatialKey]
+          val (colMin, rowMin) = {
+            val (x, y) = sourceRe.gridToMap(sourceColMin, sourceRowMin)
+            targetRe.mapToGrid(x, y)
+          }
+
+          val (colMax, rowMax) = {
+            val (x, y) = sourceRe.gridToMap(sourceColMax, sourceRowMax)
+            targetRe.mapToGrid(x, y)
+          }
+
+          KeyBounds(
+            kb.minKey.setComponent(SpatialKey(colMin, rowMin)),
+            kb.maxKey.setComponent(SpatialKey(colMax, rowMax)))
+      }
+
+    val nextMetadata =
+      rdd.metadata
+        .setComponent(nextLayout)
+        .setComponent(nextKeyBounds)
+
+    // Functions for combine step
+    def createTiles(tile: (K,  Array[Coordinate])): Seq[(K,  Array[Coordinate])]                             = Seq(tile)
+    def mergeTiles1(tiles: Seq[(K,  Array[Coordinate])], tile: (K,  Array[Coordinate])): Seq[(K,  Array[Coordinate])]         = tiles :+ tile
+    def mergeTiles2(tiles1: Seq[(K,  Array[Coordinate])], tiles2: Seq[(K,  Array[Coordinate])]): Seq[(K,  Array[Coordinate])] = tiles1 ++ tiles2
+
+    val nextRdd = {
+      val transformedRdd = rdd
+        .map { case (key, tile) =>
+          val extent = sourceLayout.mapTransform(key)
+          val newSpatialKey = nextLayout.mapTransform(extent.center)
+          (key.setComponent(newSpatialKey), (key, tile))
+        }
+
+      partitioner
+        .fold(transformedRdd.combineByKey(createTiles, mergeTiles1, mergeTiles2))(transformedRdd.combineByKey(createTiles _, mergeTiles1 _, mergeTiles2 _, _))
+        .map { case (newKey: K, seq: Seq[(K,  Array[Coordinate])]) =>
+          val pts = seq.flatMap(_._2).toArray
+          val length = pts.length
+          val by = (decimation * length).toInt
+          val newLength = length - by
+          val arr = new Array[Coordinate](newLength)
+          val dt = DelaunayTriangulation(pts)
+          dt.decimate(by)
+
+          val ps = dt.pointSet
+          cfor(0)(_ < newLength, _ + 1) { i =>
+            arr(i) = ps.getCoordinate(i)
+          }
+
+          newKey -> arr
+        }
+    }
+
+    nextZoom -> new ContextRDD(nextRdd, nextMetadata)
+  }
+
+  def up[
+    K: SpatialComponent: ClassTag,
+    M: Component[?, LayoutDefinition]: Component[?, Bounds[K]]
+  ](rdd: RDD[(K,  Array[Coordinate])] with Metadata[M],
+    layoutScheme: LayoutScheme,
+    zoom: Int
+   ): (Int, RDD[(K,  Array[Coordinate])] with Metadata[M]) =
+    up(rdd, layoutScheme, zoom, Options.DEFAULT)
+
+  def levelStream[
+    K: SpatialComponent: ClassTag,
+    M: Component[?, LayoutDefinition]: Component[?, Bounds[K]]
+  ](rdd: RDD[(K,  Array[Coordinate])] with Metadata[M],
+    layoutScheme: LayoutScheme,
+    startZoom: Int,
+    endZoom: Int,
+    options: Options
+   ): Stream[(Int, RDD[(K,  Array[Coordinate])] with Metadata[M])] =
+    (startZoom, rdd) #:: {
+      if (startZoom > endZoom) {
+        val (nextZoom, nextRdd) = Pyramid.up(rdd, layoutScheme, startZoom, options)
+        levelStream(nextRdd, layoutScheme, nextZoom, endZoom, options)
+      } else {
+        Stream.empty
+      }
+    }
+
+  def levelStream[
+    K: SpatialComponent: ClassTag,
+    M: Component[?, LayoutDefinition]: Component[?, Bounds[K]]
+  ](rdd: RDD[(K,  Array[Coordinate])] with Metadata[M],
+    layoutScheme: LayoutScheme,
+    startZoom: Int,
+    endZoom: Int
+   ): Stream[(Int, RDD[(K,  Array[Coordinate])] with Metadata[M])] =
+    levelStream(rdd, layoutScheme, startZoom, endZoom, Options.DEFAULT)
+
+  def levelStream[
+    K: SpatialComponent: ClassTag,
+    M: Component[?, LayoutDefinition]: Component[?, Bounds[K]]
+  ](rdd: RDD[(K,  Array[Coordinate])] with Metadata[M],
+    layoutScheme: LayoutScheme,
+    startZoom: Int,
+    options: Options
+   ): Stream[(Int, RDD[(K,  Array[Coordinate])] with Metadata[M])] =
+    levelStream(rdd, layoutScheme, startZoom, 0, options)
+
+  def levelStream[
+    K: SpatialComponent: ClassTag,
+    M: Component[?, LayoutDefinition]: Component[?, Bounds[K]]
+  ](rdd: RDD[(K,  Array[Coordinate])] with Metadata[M],
+    layoutScheme: LayoutScheme,
+    startZoom: Int
+   ): Stream[(Int, RDD[(K,  Array[Coordinate])] with Metadata[M])] =
+    levelStream(rdd, layoutScheme, startZoom, Options.DEFAULT)
+
+  def upLevels[
+    K: SpatialComponent: ClassTag,
+    M: Component[?, LayoutDefinition]: Component[?, Bounds[K]]
+  ](rdd: RDD[(K,  Array[Coordinate])] with Metadata[M],
+    layoutScheme: LayoutScheme,
+    startZoom: Int,
+    endZoom: Int,
+    options: Options
+   )(f: (RDD[(K,  Array[Coordinate])] with Metadata[M], Int) => Unit): RDD[(K,  Array[Coordinate])] with Metadata[M] = {
+    def runLevel(thisRdd: RDD[(K,  Array[Coordinate])] with Metadata[M], thisZoom: Int): (RDD[(K,  Array[Coordinate])] with Metadata[M], Int) =
+      if (thisZoom > endZoom) {
+        f(thisRdd, thisZoom)
+        val (nextZoom, nextRdd) = Pyramid.up(thisRdd, layoutScheme, thisZoom, options)
+        runLevel(nextRdd, nextZoom)
+      } else {
+        f(thisRdd, thisZoom)
+        (thisRdd, thisZoom)
+      }
+
+    runLevel(rdd, startZoom)._1
+  }
+
+  def upLevels[
+    K: SpatialComponent: ClassTag,
+    M: Component[?, LayoutDefinition]: Component[?, Bounds[K]]
+  ](rdd: RDD[(K,  Array[Coordinate])] with Metadata[M],
+    layoutScheme: LayoutScheme,
+    startZoom: Int,
+    endZoom: Int
+   )(f: (RDD[(K,  Array[Coordinate])] with Metadata[M], Int) => Unit): RDD[(K,  Array[Coordinate])] with Metadata[M] =
+    upLevels(rdd, layoutScheme, startZoom, endZoom, Options.DEFAULT)(f)
+
+  def upLevels[
+    K: SpatialComponent: ClassTag,
+    M: Component[?, LayoutDefinition]: Component[?, Bounds[K]]
+  ](rdd: RDD[(K,  Array[Coordinate])] with Metadata[M],
+    layoutScheme: LayoutScheme,
+    startZoom: Int,
+    options: Options
+   )(f: (RDD[(K,  Array[Coordinate])] with Metadata[M], Int) => Unit): RDD[(K,  Array[Coordinate])] with Metadata[M] =
+    upLevels(rdd, layoutScheme, startZoom, 0, options)(f)
+
+  def upLevels[
+    K: SpatialComponent: ClassTag,
+    M: Component[?, LayoutDefinition]: Component[?, Bounds[K]]
+  ](rdd: RDD[(K,  Array[Coordinate])] with Metadata[M],
+    layoutScheme: LayoutScheme,
+    startZoom: Int
+   )(f: (RDD[(K,  Array[Coordinate])] with Metadata[M], Int) => Unit): RDD[(K,  Array[Coordinate])] with Metadata[M] =
+    upLevels(rdd, layoutScheme, startZoom, Options.DEFAULT)(f)
+}
+
