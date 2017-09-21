e4056ea5fd5bdc8b52f77811422e5a9576275150
Taylor Nation <tnation14@gmail.com>
2017-09-21 15:51:17 -0400

    Add STRTA to build/publish container in CI

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -6,53 +6,32 @@ if [[ -n "${PC_DEMO_DEBUG}" ]]; then
     set -x
 fi
 
+if [[ -n "${TRAVIS_COMMIT}" ]]; then
+    VERSION="${TRAVIS_COMMIT:0:7}"
+else
+    VERSION=$(git rev-parse --short HEAD)
+fi
+
 function usage() {
     echo -n \
          "Usage: $(basename "$0")
-Build application for staging or a release.
+
+Builds container images using docker-compose.
 "
 }
 
-if [[ -n "${GIT_COMMIT}" ]]; then
-    GIT_COMMIT="${GIT_COMMIT:0:7}"
-else
-    GIT_COMMIT="$(git rev-parse --short HEAD)"
-fi
-
-if [ "${BASH_SOURCE[0]}" = "${0}" ]; then
-    if [ "${1:-}" = "--help" ]; then
+if [ "${BASH_SOURCE[0]}" = "${0}" ]
+then
+    if [ "${1:-}" = "--help" ]
+    then
         usage
     else
-        # Delete output from previous build.
-        GIT_COMMIT="${GIT_COMMIT}" docker-compose \
-                  -f docker-compose.yml \
-                  -f docker-compose.test.yml \
-                  run --entrypoint bash --rm --no-deps app-frontend \
-                  -c 'rm -rf /usr/src/dist/*'
 
-        # Build React application, which assembles the bundle within
-        # the container image.
-        GIT_COMMIT="${GIT_COMMIT}" docker-compose \
-                  -f docker-compose.yml \
-                  -f docker-compose.test.yml \
-                  run --rm --no-deps app-frontend
+        # Build React application
 
-        # Build the Nginx container image and pull in the staging area
-        # web root.
-        GIT_COMMIT="${GIT_COMMIT}" docker-compose \
-                  -f docker-compose.yml \
-                  -f docker-compose.test.yml \
-                  build nginx
-
-        ./scripts/test.sh
-
-        # If the cibuild.d directory exists, source each script it
-        # contains. This allows the core cibuild.sh to be extended with
-        # project specific scripts.
-        if [ -d "./scripts/cibuild.d" ]; then
-            for file in ./scripts/cibuild.d/*.sh; do
-                source "${file}"
-            done
-        fi
+        echo "Building static bundle..."
+        VERSION="${VERSION}" docker-compose \
+            -f docker-compose.yml \
+            run --rm --no-deps pc-assets
     fi
 fi
--- scripts/cipublish.sh
+++ scripts/cipublish.sh
@@ -6,59 +6,42 @@ if [[ -n "${PC_DEMO_DEBUG}" ]]; then
     set -x
 fi
 
-DIR="$(dirname "$0")"
+if [[ -n "${TRAVIS_COMMIT}" ]]; then
+    GIT_COMMIT="${TRAVIS_COMMIT:0:7}"
+else
+    GIT_COMMIT="$(git rev-parse --short HEAD)"
+fi
 
 function usage() {
     echo -n \
 "Usage: $(basename "$0")
-
-Publish container images to Elastic Container Registry (ECR) and
-other artifacts to S3.
+Publish images to Amazon ECR
 "
 }
-
-if [[ -n "${GIT_COMMIT}" ]]; then
-    GIT_COMMIT="${GIT_COMMIT:0:7}"
-else
-    GIT_COMMIT="$(git rev-parse --short HEAD)"
-fi
 
 if [ "${BASH_SOURCE[0]}" = "${0}" ]; then
     if [ "${1:-}" = "--help" ]; then
         usage
     else
         if [[ -n "${AWS_ECR_ENDPOINT}" ]]; then
-            # echo "Building application JAR"
-            # docker-compose \
-            #     run --rm --no-deps api-server server/clean
-            # docker-compose \
-            #     run --rm --no-deps api-server server/assembly
 
-            # Build React application, which assembles the bundle within
-            # the container image.
-            GIT_COMMIT="${GIT_COMMIT}" docker-compose \
-                      -f docker-compose.yml \
-                      -f docker-compose.test.yml \
-                      run --rm --no-deps app-frontend
+            echo "Building Pointcloud Demo Server JAR..."
+            docker-compose run --rm pc-api-server server/assembly
 
-            echo "Building container images"
-            GIT_COMMIT="${GIT_COMMIT}" docker-compose \
-                      -f "${DIR}/../docker-compose.yml" \
-                      -f "${DIR}/../docker-compose.test.yml"\
-                      build nginx api-server
-
-            # # Evaluate the return value of the get-login subcommand, which
-            # # is a docker login command with temporarily ECR credentials.
-            eval "$(aws ecr get-login --region us-east-1)"
+            echo "Building API Server and Nginx containers"
+            GIT_COMMIT="${GIT_COMMIT}" \
+            docker-compose -f docker-compose.yml \
+                           -f docker-compose.test.yml build \
+                              pc-nginx pc-api-server
+            eval "$(aws ecr get-login --no-include-email)"
+            docker tag "pointcloud-api-server:${GIT_COMMIT}" \
+                "${AWS_ECR_ENDPOINT}/pointcloud-api-server:${GIT_COMMIT}"
 
             docker tag "pointcloud-nginx:${GIT_COMMIT}" \
-                   "${AWS_ECR_ENDPOINT}/pointcloud-nginx:${GIT_COMMIT}"
-            docker tag "pointcloud-api-server:${GIT_COMMIT}" \
-                   "${AWS_ECR_ENDPOINT}/pointcloud-api-server:${GIT_COMMIT}"
+                "${AWS_ECR_ENDPOINT}/pointcloud-nginx:${GIT_COMMIT}"
 
-            docker push "${AWS_ECR_ENDPOINT}/pointcloud-nginx:${GIT_COMMIT}"
             docker push "${AWS_ECR_ENDPOINT}/pointcloud-api-server:${GIT_COMMIT}"
-
+            docker push "${AWS_ECR_ENDPOINT}/pointcloud-nginx:${GIT_COMMIT}"
         else
             echo "ERROR: No AWS_ECR_ENDPOINT variable defined."
             exit 1
